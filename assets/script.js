6b9718c3d4a7fe6ffbc741ba61179ef4abd74c6a
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-01-22 22:13:37 +1030

    fixed date coding, added weather icons, commented js file

--- assets/script.js
+++ assets/script.js
@@ -9,16 +9,17 @@ var searchBtn = document.querySelector(".search-Btn");
 var userCity = document.querySelector(".city-search");
 var forecastHeader = document.querySelector(".forecast-header");
 
-// Create new elements to be used in displaying fetched weather info
-
+// Create & style new elements to be used in displaying fetched weather info
 var cityName = document.createElement("h2");
 var temp = document.createElement("p");
+temp.classList.add("temps");
 var maxMin = document.createElement("p");
+maxMin.classList.add("temps");
 var wind = document.createElement("p");
 var humidity = document.createElement("p");
 var uvIndex = document.createElement("p");
 var icon = document.createElement("img");
-// append new elements to document
+// Append new elements to document
 city.appendChild(cityName);
 city.appendChild(icon);
 city.appendChild(temp);
@@ -27,7 +28,7 @@ city.appendChild(wind);
 city.appendChild(humidity);
 city.appendChild(uvIndex);
 
-
+//Fetch current weather api
 function getCity(city) {
     var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
     return fetch(currentWeather)
@@ -36,13 +37,14 @@ function getCity(city) {
 
 }
 
+// Fetch onecall api for forecast
 function forecast(long, lat) {
     var forecastWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=metric&appid=" + apiKey;
     return fetch(forecastWeather)
         .then(response => response.json());
 }
 
-
+// Display current weather and forecast for inputted city on click 'search'
 searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
     // get user input 
@@ -52,21 +54,23 @@ searchBtn.addEventListener("click", function (event) {
         return
     }
 
-    // pass to getcity function 
+    // User input passed to getCity function to get data of corresponding city
     getCity(search).then(function (weather) {
         console.log(weather);
+        //Display data
         cityName.textContent = weather.name + " - " + today.format("DD/MM/YY");
-        temp.textContent = "Current temperature: " + weather.main.temp + "°C"
+        icon.src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+        temp.textContent = "Currently: " + weather.main.temp + "°C";
         wind.textContent = "Wind: " + weather.wind.speed + " m/s";
         humidity.textContent = "Humidity: " + weather.main.humidity + "%";
 
-        // to get weather data
-        // display data
+        // Pass in city coordinates (obtained from current weather api fetch) then display forecast weather data
         return forecast(weather.coord.lon, weather.coord.lat)
     }).then(function (forecastData) {
         console.log(forecastData);
-        forecastHeader.textContent = "5-Day Forecast:";
+        forecastHeader.textContent = "Forecast:";
         uvIndex.textContent = "UV Index: " + forecastData.current.uvi;
+        // Colour code uv index per severity
         if (forecastData.current.uvi < 6) {
             uvIndex.setAttribute("style", "background-color: green");
         } else if (forecastData.current.uvi < 9) {
@@ -74,31 +78,42 @@ searchBtn.addEventListener("click", function (event) {
         } else {
             uvIndex.setAttribute("style", "background-color: red");
         }
+        maxMin.textContent = "Max: " + forecastData.daily[0].temp.max + "°C / Min: " + forecastData.daily[0].temp.min + "°C";
+        // Loop through 'daily' weather data from one call api to make 5 day forecast cards
         for (var i = 1; i < 6; i++) {
+            // Create & style card elements
             var forecastCard = document.createElement("div");
-            forecastCard.classList.add("forecast-card", "col");
+            forecastCard.classList.add("col", "forecast-card");
             var forecastDate = document.createElement("h4");
             var forecastIcon = document.createElement("img");
-            var forecastTemp = document.createElement("p");
+            var forecastMax = document.createElement("p");
+            var forecastMin = document.createElement("p");
+            forecastMax.classList.add("temps");
+            forecastMin.classList.add("temps");
             var forecastWind = document.createElement("p");
             var forecastHumidity = document.createElement("p");
-            // append info to card
+            // Append text elements to card
             forecastCard.appendChild(forecastDate);
             forecastCard.appendChild(forecastIcon);
-            forecastCard.appendChild(forecastTemp);
+            forecastCard.appendChild(forecastMax);
+            forecastCard.appendChild(forecastMin);
             forecastCard.appendChild(forecastWind);
             forecastCard.appendChild(forecastHumidity);
+            // Append card to forecast div
             forecastDiv.appendChild(forecastCard);
-            // set text content as fetched weather forecast data
-            var reformatDate = moment(forecastData.daily[i].dt).format("DD/MM/YY");
-            console.log(reformatDate);
+            // Set text content as fetched weather forecast data
+            var reformatDate = moment.unix(forecastData.daily[i].dt).format("DD/MM/YY");
             forecastDate.textContent = reformatDate;
-            forecastTemp.textContent = "Temperature: " + forecastData.daily[i].temp.day;
+            forecastIcon.src = "http://openweathermap.org/img/w/" + forecastData.daily[i].weather[0].icon + ".png";
+            forecastMax.textContent = "Max: " + forecastData.daily[i].temp.max + "°C";
+            forecastMin.textContent = "Min: " + forecastData.daily[i].temp.min + "°C";
             forecastWind.textContent = "Wind: " + forecastData.daily[i].wind_speed + " m/s";
             forecastHumidity.textContent = "Humidity: " + forecastData.daily[i].humidity + "%";
+            
 
 
         }
+        
 
     })
 
