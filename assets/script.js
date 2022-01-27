ac318ec25b85b488d16c4cfeb8bae0c5f8020656
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-01-27 23:56:40 +1030

    fixed issue of duplicating search history buttons

--- assets/script.js
+++ assets/script.js
@@ -80,11 +80,11 @@ function fetchForecast (search, createBtn) {
 
     // User input passed to getCity function to get data of corresponding city
     $(".forecast").empty();
+
     getCity(search).then(function (weather) {
         console.log(weather);
         if (createBtn) createHistoryButton (weather.name);
-        
-        saveSearch (weather.name);
+        if (createBtn) saveSearch (weather.name);
         //Display data
         cityName.textContent = weather.name + " - " + today.format("DD/MM/YY");
         icon.src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
@@ -160,89 +160,5 @@ searchBtn.addEventListener("click",  function(event) {
     }
     fetchForecast(search, true)
 });
-
-// Display current weather and forecast for inputted city on click 'search'
- //searchBtn.addEventListener("click",  (event) {
-//     event.preventDefault();
-//     // get user input 
-//     var search = userCity.value;
-//     if (search === "") {
-//         window.alert("Please enter a city name.")
-//         return
-        
-//     }
-
-//     // User input passed to getCity function to get data of corresponding city
-//     $(".forecast").empty();
-//     getCity(search).then(function (weather) {
-//         console.log(weather);
-//         createHistoryButton (weather.name);
-//         saveSearch (weather.name);
-//         //Display data
-//         cityName.textContent = weather.name + " - " + today.format("DD/MM/YY");
-//         icon.src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
-//         temp.textContent = "Currently: " + weather.main.temp + "°C";
-//         wind.textContent = "Wind: " + weather.wind.speed + " m/s";
-//         humidity.textContent = "Humidity: " + weather.main.humidity + "%";
-        
-
-//         // Pass in city coordinates (obtained from current weather api fetch) then display forecast weather data
-//         return forecast(weather.coord.lon, weather.coord.lat)
-//     }).then(function (forecastData) {
-//         console.log(forecastData);
-//         forecastHeader.textContent = "Forecast:";
-//         uvIndex.textContent = "UV Index: " + forecastData.current.uvi;
-//         // Colour code uv index per severity
-//         if (forecastData.current.uvi < 6) {
-//             uvIndex.setAttribute("style", "background-color: green");
-//         } else if (forecastData.current.uvi < 9) {
-//             uvIndex.setAttribute("style", "background-color: orange;",)
-//         } else {
-//             uvIndex.setAttribute("style", "background-color: red");
-//         }
-//         maxMin.textContent = "Max: " + forecastData.daily[0].temp.max + "°C / Min: " + forecastData.daily[0].temp.min + "°C";
-//         // Loop through 'daily' weather data from one call api to make 5 day forecast cards
-//         for (var i = 1; i < 6; i++) {
-            
-//             // Create & style card elements
-//             var forecastCard = document.createElement("div");
-//             forecastCard.classList.add("col", "forecast-card");
-//             var forecastDate = document.createElement("h4");
-//             var forecastIcon = document.createElement("img");
-//             var forecastMax = document.createElement("p");
-//             var forecastMin = document.createElement("p");
-//             forecastMax.classList.add("temps");
-//             forecastMin.classList.add("temps");
-//             var forecastWind = document.createElement("p");
-//             var forecastHumidity = document.createElement("p");
-//             // Append text elements to card
-//             forecastCard.appendChild(forecastDate);
-//             forecastCard.appendChild(forecastIcon);
-//             forecastCard.appendChild(forecastMax);
-//             forecastCard.appendChild(forecastMin);
-//             forecastCard.appendChild(forecastWind);
-//             forecastCard.appendChild(forecastHumidity);
-//             // Append card to forecast div
-//             forecastDiv.appendChild(forecastCard);
-//             // Set text content as fetched weather forecast data
-//             var reformatDate = moment.unix(forecastData.daily[i].dt).format("DD/MM/YY");
-//             forecastDate.textContent = reformatDate;
-//             forecastIcon.src = "http://openweathermap.org/img/w/" + forecastData.daily[i].weather[0].icon + ".png";
-//             forecastMax.textContent = "Max: " + forecastData.daily[i].temp.max + "°C";
-//             forecastMin.textContent = "Min: " + forecastData.daily[i].temp.min + "°C";
-//             forecastWind.textContent = "Wind: " + forecastData.daily[i].wind_speed + " m/s";
-//             forecastHumidity.textContent = "Humidity: " + forecastData.daily[i].humidity + "%";
-            
-
-
-//         }
-        
-
-//     })
-
-
-//})
-
-
 
 populateHistory ();
